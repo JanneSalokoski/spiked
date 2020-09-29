7c16bf208fd8b30f37f407d59bfffe8d9e310ba1
Janne Salokoski <salokoski.janne@gmail.com>
2020-09-29 11:23:40 +0300

    Properly fetch products from API

--- src/ProductView/ProductView.tsx
+++ src/ProductView/ProductView.tsx
@@ -2,19 +2,37 @@ import React, {useContext} from 'react'
 
 import "./ProductView.scss";
 
+import API from "../API/API.tsx";
+
 import {LogicContext} from '../LogicContext/LogicContext.tsx';
 
 function ProductView(props) {
 	const [logicState, logicDispatch] = useContext(LogicContext);
 
-	function handleIncrement(event) {
-		logicDispatch({type: "increment"});
+	async function handleLoadProductsClick(event) {
+		const request = new API.Get_Products();
+		const products = await request.fetch();
+		logicDispatch({type: "set_products", payload: products});
+	}
+
+	function createProductElement(productObject) {
+		return (
+			<div className="product">
+				{productObject.name}
+			</div>
+		);
+	}
+
+	function createProductElements(productObjects) {
+		return productObjects.map(productObject => createProductElement(productObject));
 	}
 
 	return (
 		<div className="ProductView">
-			<p>Count: {logicState.count}</p>
-			<button onClick={handleIncrement}>Increment</button>
+			<div>
+				{ createProductElements(logicState.products) }
+			</div>
+			<button onClick={handleLoadProductsClick}>Load products</button>
 		</div>
 	);
 }

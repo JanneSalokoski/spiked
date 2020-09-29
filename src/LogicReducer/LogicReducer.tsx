1d6c25d12ffa1c573e4ba6ed792eb95b3354ddd1
Janne Salokoski <salokoski.janne@gmail.com>
2020-09-29 11:18:19 +0300

    Implement LogicReducer in a nicer way

--- src/LogicReducer/LogicReducer.tsx
+++ src/LogicReducer/LogicReducer.tsx
@@ -1,16 +1,21 @@
 import React, {useReducer} from 'react';
 
 const initialLogicState = {
-	count: 0
+	count: 0,
+	products: []
 };
 
 function LogicReducer(state, action) {
-	switch (action.type) {
-		case 'increment':
-			return {count: state.count + 1};
-		default:
-			throw new Error();
-	}
+	let newState = {...state};
+
+	const handlers = {
+		"set_products": (payload) => newState.products = payload,
+		"set_customers": (payload) => newState.customers = payload,
+		"set_transactions": (payload) => newState.transactions = payload,
+	};	
+
+	handlers[action.type](action.payload);
+	return newState;
 }
 
 export {LogicReducer, initialLogicState};

d84021abeda6851d086b2b8cf879ae92e689bc89
Janne Salokoski <salokoski.janne@gmail.com>
2020-09-29 12:09:42 +0300

    Since this is TypeScript, use some actual TypeScript

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -1,5 +1,17 @@
-class API_request {
-	constructor(endpoint, method="GET", properties={}) {
+interface fetch_function_interface {
+	(): [key: string]: any;
+}
+
+interface API_request_interface {
+	url: string;
+	method: string;
+	properties?: [key: string]: any;
+	parameters?: [key: string]: any;
+	fetch: fetch_function_interface;
+}
+
+class API_request implements API_request_interface {
+	constructor(endpoint: string, method:string="GET", properties={}) {
 		this.url = new URL(`http://salokoski.eu/socket/${endpoint}.php`);
 		this.method = method;
 		this.properties = properties;
@@ -12,8 +24,13 @@ class API_request {
 	}
 }
 
+interface Properties_object {
+	method: "GET" | "POST";
+	body: string;
+}
+
 class API_get_request extends API_request {
-	constructor(endpoint, parameters) {
+	constructor(endpoint: string, parameters: Parameters) {
 		super(endpoint, "GET");
 
 		this.parameters = parameters;
@@ -22,15 +39,23 @@ class API_get_request extends API_request {
 	}
 }
 
-class API_post_request extends API_request {
-	constructor(endpoint, body) {
-		super(endpoint, "method");
+interface Body {
+	[key: string]: any;
+}
+
+interface API_post_request_inteface extends API_request_interface {
+		get_properties_object: (): Properties_object;	
+}
+
+class API_post_request extends API_request implements API_post_request_inteface {
+	constructor(endpoint: string, body: Body) {
+		super(endpoint, "POST");
 
 		this.body = body;
 		this.properties = this.get_properties_object();
 	}
 
-	get_properties_object() {
+	get_properties_object(): Properties_object {
 		return {
 			method: "POST",
 			body: JSON.stringify(this.body)
@@ -47,13 +72,13 @@ class Get_Products_API_Request extends API_get_request {
 }
 
 class Get_Product_API_Request extends API_get_request {
-	constructor(id) {
+	constructor(id: number) {
 		super("get_product", {id: id});
 	}
 }
 
 class Set_Product_API_Request extends API_post_request {
-	constructor(body) {
+	constructor(body: [key: string]: any) {
 		super("set_product", body);
 	}
 }
@@ -67,13 +92,13 @@ class Get_Customers_API_Request extends API_get_request {
 }
 
 class Get_Customer_API_Request extends API_get_request {
-	constructor(id) {
+	constructor(id: number) {
 		super("get_customer", {id: id});
 	}
 }
 
 class Set_Customer_API_Request extends API_post_request {
-	constructor(body) {
+	constructor(body: [key: string]: any) {
 		super("set_customer", body);
 	}
 }
@@ -87,17 +112,16 @@ class Get_Transactions_API_Request extends API_get_request {
 }
 
 class Get_Transaction_API_Request extends API_get_request {
-	constructor(id) {
+	constructor(id: number) {
 		super("get_transaction", {id: id});
 	}
 }
 
 class Set_Transaction_API_Request extends API_post_request {
-	constructor(body) {
+	constructor(body: [key: string]: any) {
 		super("set_transaction", body);
 	}
 }
-
 
 class API {
 	constructor() {

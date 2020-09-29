3afde08406c2affdfe298510c8d302a71a7780f6
Janne Salokoski <salokoski.janne@gmail.com>
2020-09-29 10:53:17 +0300

    Implement API.tsx in a more OOP way. Don't know if this is any good, but...

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -20,17 +20,121 @@ async function post_request(endpoint, payload) {
 	return data;
 }
 
-const API = {
-	get_products: async () => await get_request("get_product"),
-	get_product: async (id) => await get_request("get_product", {id: id}),
-	get_customers: async () => await get_request("get_product"),
-	get_customer: async (id) => await get_request("get_product", {id: id}),
-	get_transactions: async () => await get_request("get_product"),
-	get_transaction: async (id) => await get_request("get_product", {id: id}),
+class API_request {
+	constructor(endpoint, method="GET", properties={}) {
+		this.url = new URL(`${BASE_URL}/${endpoint}.php`);
+		this.method = method;
+		this.properties = properties;
+	}
 
-	set_product: async () => await post_request("set_product", properties),
-	set_customer: async () => await post_request("set_product", properties),
-	set_transaction: async () => await post_request("set_product", properties),
-};
+	async fetch() {
+		const response = await fetch(this.url, this.properties);
+		const data = await response.json();
+		return data;
+	}
+}
 
-export default API;
+class API_get_request extends API_request {
+	constructor(endpoint, parameters) {
+		super(endpoint, "GET");
+
+		this.parameters = parameters;
+		this.url.search = new URLSearchParams(this.parameters);
+		this.properties = {method: "GET"};
+	}
+}
+
+class API_post_request extends API_request {
+	constructor(endpoint, body) {
+		super(endpoint, "method");
+
+		this.body = body;
+		this.properties = this.get_properties_object();
+	}
+
+	get_properties_object() {
+		return {
+			method: "POST",
+			body: JSON.stringify(this.body)
+		};
+	}	
+}
+
+// Product API calls
+
+class Get_Products_API_Request extends API_get_request {
+	constructor() {
+		super("get_products");
+	}
+}
+
+class Get_Product_API_Request extends API_get_request {
+	constructor(id) {
+		super("get_product", {id: id});
+	}
+}
+
+class Set_Product_API_Request extends API_post_request {
+	constructor(body) {
+		super("set_product", body);
+	}
+}
+
+// Customer API calls
+
+class Get_Customers_API_Request extends API_get_request {
+	constructor() {
+		super("get_customers");
+	}
+}
+
+class Get_Customer_API_Request extends API_get_request {
+	constructor(id) {
+		super("get_customer", {id: id});
+	}
+}
+
+class Set_Customer_API_Request extends API_post_request {
+	constructor(body) {
+		super("set_customer", body);
+	}
+}
+
+// Transaction API calls
+
+class Get_Transactions_API_Request extends API_get_request {
+	constructor() {
+		super("get_transactions");
+	}
+}
+
+class Get_Transaction_API_Request extends API_get_request {
+	constructor(id) {
+		super("get_transaction", {id: id});
+	}
+}
+
+class Set_Transaction_API_Request extends API_post_request {
+	constructor(body) {
+		super("set_transaction", body);
+	}
+}
+
+
+class API {
+	constructor() {
+		this.Get_Products = Get_Products_API_Request;
+		this.Get_Product = Get_Product_API_Request;
+		this.Set_Product = Set_Product_API_Request;
+
+		this.Get_Customers = Get_Customers_API_Request;
+		this.Get_Customer = Get_Customer_API_Request;
+		this.Set_Customer = Set_Customer_API_Request;
+
+		this.Get_Transactions = Get_Transactions_API_Request;
+		this.Get_Transaction = Get_Transaction_API_Request;
+		this.Set_Transaction = Set_Transaction_API_Request;
+	}
+}
+
+export default new API();
